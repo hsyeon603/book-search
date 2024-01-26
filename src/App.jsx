203f08e0938539e5f2f9bf00b2eae219485e6fc8
hsyeon603 <hsyeon603@gmail.com>
2024-01-26 20:03:03 +0900

    ✨ Integration with book search API by Google Books  & Add props to Book component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from 'react';
+import Book from './components/Book.jsx';
+
 function App() {
-  return <></>;
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
+  const getBooks = async () => {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=flower`);
+    const json = await response.json();
+    setBooks(json.items);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {books.map((book) => {
+            let id = book.id;
+            let { imageLinks, title } = book.volumeInfo;
+            let img = imageLinks.thumbnail ? imageLinks.thumbnail : '';
+            return <Book key={id} title={title} img={img} />;
+          })}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
