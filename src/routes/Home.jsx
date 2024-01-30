185aa2addf98901935fb9f511ac33c9181d0b086
hsyeon603 <hsyeon603@gmail.com>
2024-01-30 20:23:00 +0900

    ✨ Made drawPage function

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,35 +1,45 @@
 import { useState, useEffect } from 'react';
 import Book from '../components/Book.jsx';
+import Search from '../components/Search.jsx';
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const [books, setBooks] = useState([]);
-  const getBooks = async () => {
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=flower`);
+  const getBooks = async (keyword) => {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword}`);
     const json = await response.json();
     setBooks(json.items);
     setLoading(false);
   };
 
+  const drawPage = () => {
+    setLoading(true);
+    // TODO ui 구성 후 loading 상태 변경
+    setLoading(false);
+  };
+
   useEffect(() => {
-    getBooks();
-  }, []);
+    drawPage();
+  }, [books]);
 
   return (
     <>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
-          {books.map((book) => {
-            let id = book.id;
-            let { imageLinks, title } = book.volumeInfo;
-            let img = imageLinks?.thumbnail
-              ? imageLinks.thumbnail
-              : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png?20200912122019';
-            return <Book key={id} id={id} title={title} img={img} />;
-          })}
-        </div>
+        <>
+          <Search handleClick={getBooks} />
+          <div>
+            {books.map((book) => {
+              let id = book.id;
+              let { imageLinks, title } = book.volumeInfo;
+              let img = imageLinks?.thumbnail
+                ? imageLinks.thumbnail
+                : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png?20200912122019';
+              return <Book key={id} id={id} title={title} img={img} />;
+            })}
+          </div>
+        </>
       )}
     </>
   );
