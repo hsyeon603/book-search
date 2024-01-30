fb43f0c751935a9786522554b75104484c93ec48
hsyeon603 <hsyeon603@gmail.com>
2024-01-30 22:54:10 +0900

    ✨ Implement infinite search feature

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,23 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useRef, useEffect } from 'react';
 
-export default function Book({ id, img, title }) {
+export default function Book({ id, img, title, isObserveTarget, handleScroll }) {
+  let bookWrapper = useRef();
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) handleScroll(true);
+    });
+  });
+
+  useEffect(() => {
+    if (isObserveTarget) observer.observe(bookWrapper.current);
+  }, bookWrapper.current);
+
   return (
-    <div>
+    <div ref={bookWrapper}>
       <Link to={`/book/${id}`}>
-        <img src={img} />
+        <img src={img} className="skeleton" />
         <h2>{title}</h2>
       </Link>
     </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react';
-import Book from '../components/Book.jsx';
 import Search from '../components/Search.jsx';
+import BookList from '../components/BookList.jsx';
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const [books, setBooks] = useState([]);
-  const getBooks = async (keyword) => {
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword}`);
-    const json = await response.json();
-    setBooks(json.items);
-    setLoading(false);
+  const [searchWord, setSearchWord] = useState('');
+  const [isBottom, setIsBottom] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const getBooks = async (word) => {
+    if (word) {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${word}&startIndex=${index}`);
+      const json = await response.json();
+      setBooks((prev) => [...prev, ...json.items]);
+      setLoading(false);
+      setIndex((prev) => (prev += 10));
+      setIsBottom(false);
+    }
   };
 
   const drawPage = () => {
@@ -22,23 +30,22 @@ export default function Home() {
     drawPage();
   }, [books]);
 
+  useEffect(() => {
+    getBooks(searchWord);
+  }, [searchWord]);
+
+  useEffect(() => {
+    if (isBottom) getBooks(searchWord);
+  }, [isBottom]);
+
   return (
     <>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
         <>
-          <Search handleClick={getBooks} />
-          <div>
-            {books.map((book) => {
-              let id = book.id;
-              let { imageLinks, title } = book.volumeInfo;
-              let img = imageLinks?.thumbnail
-                ? imageLinks.thumbnail
-                : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png?20200912122019';
-              return <Book key={id} id={id} title={title} img={img} />;
-            })}
-          </div>
+          <Search handleClick={setSearchWord} />
+          <BookList books={books} handleScroll={setIsBottom} />
         </>
       )}
     </>
