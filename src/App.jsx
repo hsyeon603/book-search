5a656f8a218429fb89bfcaf5a6bd3845e1eac717
hsyeon603 <hsyeon603@gmail.com>
2024-02-21 14:50:27 +0900

    🎨  Add logo img

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import './styles/main.scss';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/book/:id" element={<Detail />} />
-      </Routes>
-    </Router>
+    <>
+      <img className="logo" src="/src/assets/images/logo_no-background.png" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:id" element={<Detail />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
