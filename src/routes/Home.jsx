d36e5715413322dbbd973f1df4fa035b4001992b
hsyeon603 <hsyeon603@gmail.com>
2024-02-22 14:32:07 +0900

    🔐 Add .env file to protect api key && update Home component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,7 +10,9 @@ export default function Home() {
 
   const getBooks = async () => {
     const response = await fetch(
-      `http://data4library.kr/api/loanItemSrch?authKey=5a615fb780fec17d6cffaee00487472e711a950d887996a209599630893fd7eb&startDt=2023-09-01&endDt=2023-12-31&addCode=0&pageNo=${index}&pageSize=10`
+      `http://data4library.kr/api/loanItemSrch?authKey=${
+        import.meta.env.VITE_API_KEY
+      }&startDt=2023-09-01&endDt=2023-12-31&addCode=0&pageNo=${index}&pageSize=10`
     );
     const text = await response.text();
     const jsonString = xml2json(text, { compact: true, spaces: 4 });
