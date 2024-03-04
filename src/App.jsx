f99bb758447995322e296e010fe046a7c358a575
hsyeon603 <hsyeon603@gmail.com>
2024-03-04 15:39:57 +0900

    ✨ Implement logo link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './routes/Home.jsx';
 import Detail from './routes/Detail.jsx';
 import './styles/main.scss';
@@ -7,8 +7,12 @@ import Recommend from './routes/Recommend.jsx';
 export default function App() {
   return (
     <>
-      <img className="logo" src="/src/assets/images/logo_no-background.png" />
       <Router>
+        <Link to={'/'}>
+          <div className="logo-container">
+            <img className="logo" src="/src/assets/images/logo-color.png" />
+          </div>
+        </Link>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/book/:id" element={<Detail />} />
